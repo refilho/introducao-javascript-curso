3c6ec9a774b5673299853edb1b27063862234ebf
Renan Filho <refilho@gmail.com>
2019-03-21 19:53:36 -0300

    introducao-javascript-curso
    introducao-javascript-curso

---
+++ introducao-javascript/js/remover-paciente.js
@@ -0,0 +1,22 @@
+var pacientes = document.querySelectorAll(".paciente");
+
+var tabela = document.querySelector("table");
+
+tabela.addEventListener("dblclick", function(event){
+	
+	event.target.parentNode.classList.add("fadeOut");
+	
+	setTimeout(function(){
+		event.target.parentNode.remove();		
+	},500);		
+});
+
+
+/*
+pacientes.forEach(function(paciente){
+	paciente.addEventListener("dblclick", function(){
+		console.log("Fui clicado com o dupli clique");
+		this.remove();
+	});
+});
+*/
